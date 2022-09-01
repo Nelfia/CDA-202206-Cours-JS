0d955e71e8b651efccbed6d6bb35740bec540145
Nelfia <qdecapele@gmail.com>
2022-09-01 15:43:09 +0200

    Exercice sur écouteurs événements. Créer un nouveau rond à l'endroit et au moment où on clique sur la souris.

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const randomNumber = (min, max) => {
     return min + (Math.floor(Math.random() * (max - min)))
 }
 
-bodyElt.addEventListener('keydown', (e) => {
-    if(e.key == 'Enter'){
-        bodyElt.style.background = `rgb(${randomNumber(0, 256)}, ${randomNumber(0,256)}, ${randomNumber(0, 256)})`
-    }
+bodyElt.addEventListener('mousedown', (e) => {
+    console.log(e)
+    let rond = document.createElement('div')
+    rond.style.backgroundColor = `rgb(${randomNumber(0, 256)}, ${randomNumber(0,256)}, ${randomNumber(0, 256)})`
+    rond.style.top = (e.y - 25) + 'px'
+    rond.style.left = (e.x - 25) + 'px'
+    bodyElt.appendChild(rond)
+    isDrawing = true
 })
