3b2894f800b96e336cd8cd9843941286d8b0e302
Nelfia <qdecapele@gmail.com>
2022-09-08 15:47:09 +0200

    Programme de combat entre compagnies opérationnel

--- app.js
+++ app.js
@@ -1,128 +1,165 @@
-
-// HTML ELEMENTS CONSTANTES ------------------------------------------------------------
-const body = document.querySelector('body')
-const articlesContainer = document.querySelector('#articles-container')
-const precedent = document.querySelector('#precedent')
-const suivant = document.querySelector('#suivant')
-const slides = document.querySelector(`#slides`)
-
-// VARIABLES ------------------------------------------------------------
-let articles = []
-let currentArticle = 0
-let slidesList = []
-
-// CLASSES -------------------------------------------------------------
-class Article {
-    constructor(titre, resume, img) {
-        this.titre = titre
-        this.resume = resume
-        this.img = img
+class Guerrier {
+    constructor(nom, arme = 'épée', armure = 5, degats = 10) {
+        this.type = this.constructor.name
+        this.nom = nom
+        this.arme = arme
+        this.armure = armure
+        this.degats = degats
+        this.pv = 100
     }
 
-    creerArticle = () => {
-        const articleElt = document.createElement('article')
-        articleElt.style.backgroundImage = `url(${this.img})`
-        articlesContainer.appendChild(articleElt)
-
-        const h2 = document.createElement('h2')
-        h2.innerHTML = `${this.titre}`
-        articleElt.appendChild(h2)
-
-        const p = document.createElement('p')
-        p.innerHTML = `${this.resume}`
-        articleElt.appendChild(p)
+    sePresenter = () => {
+        console.log(`Je suis ${this.nom} de la classe des ${this.type} et j'attaque avec mon ${this.arme}.`)
     }
-}
 
-// FONCTIONS -----------------------------------------------------------
-// Réduire la taille du point de positionnement
-const reduireSlideCursor = () => {
-    slidesList[currentArticle].style.width = '10px'
-    slidesList[currentArticle].style.height = '10px'
-}
+    isDead = () => {
+        if (this.pv <= 0) return true
+        else return false
+    }
 
-// Augmenter la taille du point de positionnement
-const augmenterSlideCursor = () => {
-    slidesList[currentArticle].style.width = '20px'
-    slidesList[currentArticle].style.height = '20px'
-}
+    attaquer = (cible) => {
+        console.log(`le ${this.type} ${this.nom} combat le ${cible.type} ${cible.nom} `)
+        cible.pv-= this.degats - cible.armure
+    }
 
-// MAJ affichage currentArticle
-const majCurrentArticle = () => {
-    articlesContainer.style.left = `${0 - (currentArticle) * 68}vw`
-}
+    static combat = (guerrier1, guerrier2) => {
+        while ((!guerrier1.isDead()) && (!guerrier2.isDead())) {
 
-// Afficher l'article suivant
-const allerArticleSuivant = () => {
-    reduireSlideCursor()
-    if (currentArticle == articles.length - 1) currentArticle = 0
-    else currentArticle++
-    majCurrentArticle()
-    augmenterSlideCursor()
-}
-
-
-// ====================== DEBUT DU PROGRAMME =================================
-
-// DONNEES - Création des Articles
-let article1 = new Article('Artemis : pourquoi veut-on renvoyer des humains sur la Lune ?', 'La mission Artemis, qui prévoit le retour de l’humain sur la Lune, devait débuter ce lundi 29 août ...', 'https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/08/as12-47-6897hrweb-5dd3-750x410.jpeg')
-articles.push(article1)
-
-let article2 = new Article('Est-il possible de tordre l’espace-temps à l’aide du warp drive ?', `Et si, au lieu de se déplacer lui-même sur des années-lumière, l'humain rapprochait simplement sa destination… en contractant l'espace-temps ? `, 'https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/07/30005_1754559_k2_k1_4033944-750x410.jpg')
-articles.push(article2)
-
-let article3 = new Article('Réchauffement climatique : notre corps pourra-t-il supporter les augmentations de température ?', 'La température à la surface du globe continue d’augmenter. Le GIEC prévoit un réchauffement planétaire de 1,5°C d’ici 2030 par rapport à l’ère préindustrielle...', `https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/08/temperature-chaleur-humain-regulation-750x410.jpg`)
-articles.push(article3)
-
-let article4 = new Article('Au Chili, le sol s’est ouvert sous nos pieds', 'Le 30 Juillet dernier, dans la petite ville de Tierra Amarilla au Nord du Chili, le sol s’est effondré, formant un énorme cratère si profond qu’on n’en voyait pas le fond....', `https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/08/fze5taxxeae4nsi-653x410.png`)
-articles.push(article4)
-
-
-// Création des éléments et affichage dans la page
-if(articles.length > 0) {
-    articles.forEach((article, key) => {
-        article.creerArticle()
-        
-        // Création des points de positionnement
-        let div = document.createElement('div')
-        div.classList.add('slide')
-        div.dataset.index = key
-        // Au clic sur un des points, changement sur la bonne slide
-        div.onclick = (e) => {
-            reduireSlideCursor()
-            let index = div.dataset.index
-            currentArticle = index
-            majCurrentArticle()
-            augmenterSlideCursor()
+            guerrier1.attaquer(guerrier2)
+            if (guerrier2.isDead()) return guerrier2
+            guerrier2.attaquer(guerrier1)
+           if (guerrier1.isDead()) return guerrier1
+            
+            console.log(`${guerrier1.nom} a ${guerrier1.pv}pv `)
+            console.log(`${guerrier2.nom} a ${guerrier2.pv}pv `)
         }
-        slidesList.push(div)
-        slides.appendChild(div)
-    })
-} else throw ('Aucun article dans le tableau articles')
-augmenterSlideCursor()
-
-suivant.onclick = () => {
-    allerArticleSuivant()
-}
-
-precedent.onclick = () => {
-    reduireSlideCursor()
-    if (currentArticle == 0) currentArticle = (articles.length - 1)
-    else currentArticle--
-    majCurrentArticle()
-    augmenterSlideCursor()
-}
-
-setInterval(() => {
-    allerArticleSuivant()
-}, 3000)
-
-body.onkeyup = (e) => {
-    if (e.key === " ") {
-        e.preventDefault()
-        reduireSlideCursor()
-        currentArticle = 0
-        majCurrentArticle()
-        augmenterSlideCursor()
     }
 }
+
+
+class Chevalier extends Guerrier {
+    constructor(nom) {
+        super(nom, 'Espadon', 10 , 12)
+    }
+}
+
+class Samourai extends Guerrier {
+    constructor(nom) {
+        super(nom, 'Katana', 5, 20)
+    }
+}
+
+class Viking extends Guerrier {
+    constructor(nom) {
+        super(nom, 'Hache', 7 , 14)
+    }
+}
+
+
+class Compagnie {
+    constructor(nom) {
+        this.nom = nom
+        this.guerriers = []
+        this.nombreDeGuerriers = 0  
+    }
+
+    sePresente = () => {
+        console.log(`Dans la compagnie ${this.nom}, nous comptons actuellement ${this.nombreDeGuerriers} guerriers vivants dans nos effectifs `)
+    }
+
+    afficherGuerriers = () => {
+        this.guerriers.forEach(guerrier => {
+            console.log(guerrier)
+        })
+    }
+
+    guerrierAppartient = (guerrier) => {
+        return this.guerriers.includes(guerrier)
+    }
+
+    ajouterGuerrier = (guerrier) => {
+        if (!this.guerrierAppartient(guerrier)) {
+            this.guerriers.push(guerrier)
+            this.nombreDeGuerriers++
+        }
+        
+    }
+
+    supprimerGuerrier = (guerrier) => {
+        if (this.guerrierAppartient(guerrier)) {
+            let index = this.guerriers.indexOf(guerrier)
+            this.guerriers.splice(index, 1)
+            this.nombreDeGuerriers--
+        }
+    }
+
+    static bataille = (compagnie1, compagnie2) => {
+        let currentGuerrier1 = compagnie1.guerriers[0]
+        let currentGuerrier2 = compagnie2.guerriers[0]
+        let compagniePerdante
+        let compagnieGagnante
+
+        console.log(`Nombre de guerriers dans ${compagnie1.nom} : ${compagnie1.nombreDeGuerriers}`)
+        console.log(`Nombre de guerriers dans ${compagnie2.nom} : ${compagnie2.nombreDeGuerriers}`)
+
+        while ((compagnie1.nombreDeGuerriers > 0) && (compagnie2.nombreDeGuerriers > 0)) {
+            let perdant = Guerrier.combat(currentGuerrier1, currentGuerrier2)
+            console.log(`Nombre de guerriers dans ${compagnie1.nom} : ${compagnie1.nombreDeGuerriers}`)
+            console.log(`Nombre de guerriers dans ${compagnie2.nom} : ${compagnie2.nombreDeGuerriers}`)
+
+            if (compagnie1.guerrierAppartient(perdant)) {
+                compagnie1.supprimerGuerrier(perdant)
+
+                if (compagnie1.nombreDeGuerriers > 0) currentGuerrier1 = compagnie1.guerriers[0]
+                else {
+                    compagniePerdante = compagnie1
+                    compagnieGagnante = compagnie2
+                }
+
+            } else {
+                compagnie2.supprimerGuerrier(perdant)
+                if (compagnie2.nombreDeGuerriers > 0) currentGuerrier2 = compagnie2.guerriers[0]
+                else {
+                    compagniePerdante = compagnie2
+                    compagnieGagnante = compagnie1
+                }
+            } 
+        }
+        console.log(`${compagniePerdante.nom} n'a plus de guerrier en vie. ${compagnieGagnante.nom} a remporté le combat !!`)
+    }
+}
+
+let perceval = new Chevalier('Perceval')
+let caradoque = new Chevalier('Caradoque')
+let lancelot = new Chevalier('Lancelot')
+let leodagan = new Chevalier('Léodagan')
+let shang = new Samourai('Shang')
+let mulan = new Samourai('Mulan')
+let moushou = new Samourai('Moushou')
+let harald = new Viking('Harald')
+let astrid = new Viking('Astrid')
+let liv = new Viking('Liv')
+let floki = new Viking('Floki')
+let thor = new Viking('Thor')
+
+let huns = new Compagnie('Huns')
+huns.ajouterGuerrier(perceval)
+huns.ajouterGuerrier(lancelot)
+huns.ajouterGuerrier(mulan)
+huns.ajouterGuerrier(harald)
+huns.ajouterGuerrier(liv)
+huns.ajouterGuerrier(thor)
+huns.sePresente()
+huns.afficherGuerriers()
+
+let hotres = new Compagnie('Hotres')
+hotres.ajouterGuerrier(caradoque)
+hotres.ajouterGuerrier(leodagan)
+hotres.ajouterGuerrier(moushou)
+hotres.ajouterGuerrier(astrid)
+hotres.ajouterGuerrier(floki)
+hotres.ajouterGuerrier(shang)
+hotres.sePresente()
+hotres.afficherGuerriers()
+
+Compagnie.bataille(hotres, huns)
