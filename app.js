400ec177f5e9afab74cc3813b655371f623bef4c
Nelfia <qdecapele@gmail.com>
2022-09-09 21:02:33 +0200

    Exercice Barre de progression OK

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const pageHeight = document.documentElement.scrollHeight
-console.log(pageHeight)
 const windowHeight = window.innerHeight
-console.log(scrollY)
-console.log(windowHeight)
+const maxHeight = pageHeight - windowHeight
+const barProgressElt = document.querySelector('#bar-progress')
+
+// Fait grandir la barre de progression du bas de la page en fonction de l'endroit où on se situe dans l'article
+document.body.onscroll = () => {
+    barProgressElt.style.width = `${Math.round(window.scrollY/maxHeight*100)}%`
+}
