978e0624d6fef99135377d18a40228ddb309c40d
Nelfia <qdecapele@gmail.com>
2022-09-09 15:10:30 +0200

    Création de la page + un peu de style

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-let reponse; 
-
-fetch("http://api.learn.pierre-godino.com/api/toto")
-.then((response) => response.text())
-.then((data) => {
-    const h1 =  document.querySelector('h1')
-    h1.innerHTML = data
-})
-.catch((error) => console.log("erreur dans le fetch :" + error))
-
-
+const pageHeight = document.documentElement.scrollHeight
+console.log(pageHeight)
+const windowHeight = window.innerHeight
+console.log(windowHeight)
