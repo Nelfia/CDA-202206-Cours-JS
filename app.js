f8dfc8fb62a85766ad560fb898bdcef09636b419
Nelfia <qdecapele@gmail.com>
2022-09-09 10:34:07 +0200

    Un peu de style et de mise en page

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
+const compagnieContainer = document.querySelector('.compagnie-container')
+
 class Guerrier {
-    constructor(nom, arme = 'épée', armure = 5, degats = 10) {
+    constructor(nom, arme = 'épée', armure = 5, degats = 10, img) {
         this.type = this.constructor.name
         this.nom = nom
         this.arme = arme
         this.armure = armure
         this.degats = degats
         this.pv = 100
+        this.img = img
     }
 
     sePresenter = () => {
         console.log(`Je suis ${this.nom} de la classe des ${this.type} et j'attaque avec mon ${this.arme}.`)
     }
 
-    isDead = () => {
-        if (this.pv <= 0) return true
-        else return false
-    }
+    isDead = () => this.pv <= 0
 
     attaquer = (cible) => {
         console.log(`le ${this.type} ${this.nom} combat le ${cible.type} ${cible.nom} `)
@@ -36,25 +36,23 @@ class Guerrier {
     }
 }
 
-
 class Chevalier extends Guerrier {
-    constructor(nom) {
-        super(nom, 'Espadon', 10 , 12)
+    constructor(nom, img) {
+        super(nom, 'Espadon', 10 , 12, img)
     }
 }
 
 class Samourai extends Guerrier {
-    constructor(nom) {
-        super(nom, 'Katana', 5, 20)
+    constructor(nom, img) {
+        super(nom, 'Katana', 5, 20, img)
     }
 }
 
 class Viking extends Guerrier {
-    constructor(nom) {
-        super(nom, 'Hache', 7 , 14)
+    constructor(nom, img) {
+        super(nom, 'Hache', 7 , 14, img)
     }
 }
-
 
 class Compagnie {
     constructor(nom) {
@@ -65,6 +63,25 @@ class Compagnie {
 
     sePresente = () => {
         console.log(`Dans la compagnie ${this.nom}, nous comptons actuellement ${this.nombreDeGuerriers} guerriers vivants dans nos effectifs `)
+    }
+
+    afficherCompagnie = () => {
+        const compagnie = document.createElement('div')
+        const compagnieNom = document.createElement('h2')
+        compagnieNom.textContent = this.nom
+        compagnie.classList.add('compagnie')
+        compagnie.appendChild(compagnieNom)
+        this.guerriers.forEach( guerrier => {
+            const divElt = document.createElement('div')
+            divElt.classList.add('guerrier')
+            divElt.innerHTML = ` <h3>${guerrier.nom}</h3>`
+            divElt.style.backgroundImage = `url("${guerrier.img}")`
+            divElt.style.backgroundRepeat = 'no-repeat'
+            divElt.style.backgroundSize = 'cover'
+            divElt.style.backgroundPosition = 'center top'
+            compagnie.appendChild(divElt)
+        })
+        compagnieContainer.appendChild(compagnie)
     }
 
     afficherGuerriers = () => {
@@ -129,28 +146,33 @@ class Compagnie {
     }
 }
 
-let perceval = new Chevalier('Perceval')
-let caradoque = new Chevalier('Caradoque')
-let lancelot = new Chevalier('Lancelot')
-let leodagan = new Chevalier('Léodagan')
-let shang = new Samourai('Shang')
-let mulan = new Samourai('Mulan')
-let moushou = new Samourai('Moushou')
-let harald = new Viking('Harald')
-let astrid = new Viking('Astrid')
-let liv = new Viking('Liv')
-let floki = new Viking('Floki')
-let thor = new Viking('Thor')
+
+
+// DONNEES ----------------------------------------------------------------
+
+let perceval = new Chevalier('Perceval', 'https://dnfx0kvkzsynw.cloudfront.net/websites/25/personnages/perceval.jpg')
+let caradoque = new Chevalier('Karadoque', 'https://dnfx0kvkzsynw.cloudfront.net/websites/25/personnages/karadoc.jpg')
+let lancelot = new Chevalier('Lancelot', 'https://i.skyrock.net/4092/16894092/pics/463774427.jpg')
+let leodagan = new Chevalier('Léodagan', 'https://www.serieously.com/app/uploads/2021/08/sans-titre-58-51-1024x577.jpg')
+let shang = new Samourai('Shang', 'https://assets.teenvogue.com/photos/5ad797b173a9c74b36114302/2:3/w_1149,h_1724,c_limit/live-action-mulan-to-not-feature-li-shang.jpg')
+let mulan = new Samourai('Mulan', 'https://static.lpnt.fr/images/2017/11/30/11512384lpw-11512587-article-jpg_4806185_1250x625.jpg')
+let moushou = new Samourai('Mushu', 'https://i.ytimg.com/vi/LL06jvZP94U/maxresdefault.jpg')
+let harold = new Viking('Harold', 'https://2img.net/h/media.npr.org/assets/img/2014/06/12/dragon-picture1_sq-cbfb5c7e7438238eb2367b04593b8e108ab9c9b5.jpg')
+let astrid = new Viking('Astrid', 'https://ae01.alicdn.com/kf/HTB1mmDzOVXXXXbzXXXXq6xXFXXX7/Papier-peint-sur-toile-personnalis-Art-Astrid-affiche-Train-votre-Dragon-comment-former-votre-Dragon-Stickers.jpg')
+let liv = new Viking('Liv', 'https://i.skyrock.net/6692/4386692/pics/3209040653_1_6_pYwcysf8.jpg')
+let floki = new Viking('Floki', 'https://i.pinimg.com/originals/69/fe/13/69fe13d90e2f4236fcb7182cd77767b2.jpg')
+let thor = new Viking('Thor', 'https://clipartix.com/wp-content/uploads/2018/03/thor-clipart-2018-23.jpg')
 
 let huns = new Compagnie('Huns')
 huns.ajouterGuerrier(perceval)
 huns.ajouterGuerrier(lancelot)
 huns.ajouterGuerrier(mulan)
-huns.ajouterGuerrier(harald)
+huns.ajouterGuerrier(harold)
 huns.ajouterGuerrier(liv)
 huns.ajouterGuerrier(thor)
 huns.sePresente()
 huns.afficherGuerriers()
+huns.afficherCompagnie()
 
 let hotres = new Compagnie('Hotres')
 hotres.ajouterGuerrier(caradoque)
@@ -161,5 +183,6 @@ hotres.ajouterGuerrier(floki)
 hotres.ajouterGuerrier(shang)
 hotres.sePresente()
 hotres.afficherGuerriers()
+hotres.afficherCompagnie()
 
 Compagnie.bataille(hotres, huns)
