b38b6de29200613363f42c9ff6c744573cb0e2e7
Nelfia <qdecapele@gmail.com>
2022-09-09 15:17:37 +0200

    Récupération de la longitude et de la lattitude de l'API

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-let reponse; 
-
-fetch("http://api.learn.pierre-godino.com/api/toto")
-.then((response) => response.text())
+fetch("http://api.learn.pierre-godino.com/api/myIP")
+.then((response) => response.json())
 .then((data) => {
-    const h1 =  document.querySelector('h1')
-    h1.innerHTML = data
+    const h1 = document.querySelector('h1')
+    h1.innerHTML = `
+        Lattitude: ${data.location.lat}<br />
+        Longitude: ${data.location.lon}
+    `
+    console.log(data.location.lat)
 })
 .catch((error) => console.log("erreur dans le fetch :" + error))
+
 
 
