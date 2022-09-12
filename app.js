d6571ad31a7194bec4ec2ea669448fc85ecad80d
Nelfia <qdecapele@gmail.com>
2022-09-12 15:03:16 +0200

    Récupération de différentes données en fonction de données reçue. Requêtes en cascade

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-const body = document.querySelector('body')
+const book1Elt = document.querySelector('.book1')
+const starkAllegiance = document.querySelector('.stark')
+const lannisterElt = document.querySelector('.lannister')
 
 const fetchData = () => {
-    fetch('https://randomfox.ca/floof')
+    fetch("https://www.anapioficeandfire.com/api/books/1")
         .then((response) => response.json())
-        .then((data) => {
-            let nouvelleImg = document.createElement('img')
-            nouvelleImg.src = data.image
-            let lien = document.createElement('a')
-            lien.href = data.link
-            lien.appendChild(nouvelleImg)
-            body.appendChild(lien)
+        .then(bookData => {
+            bookData.characters.forEach(character => {
+                fetch(character)
+                    .then((response1) => response1.json())
+                    .then(characterData => {
+                        const liElt = document.createElement('li')
+                        let name = characterData.name ? characterData.name : characterData.aliases[0]
+                        liElt.textContent = name
+                        book1Elt.appendChild(liElt)
+
+                        let allegeance = characterData.allegiances
+
+                        if (allegeance[0] === "https://anapioficeandfire.com/api/houses/362") {
+                            starkAllegiance.appendChild(liElt)
+                        } 
+                        else if ((allegeance[0] === "https://anapioficeandfire.com/api/houses/229") && !character.died) lannisterElt.appendChild(liElt)
+
+                    })
+                    .catch((error) => {throw error})
+            })
         })
-        .catch((error) => {throw error})
+        .catch(error => { throw error })
 }
 
-setInterval(fetchData, 1000)
+fetchData()
