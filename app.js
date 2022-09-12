bcef515a6b3b2b0727cef52434982b133211e89e
Nelfia <qdecapele@gmail.com>
2022-09-12 18:03:54 +0200

    Exercice OK - Drapeaux récupérés et affichés.

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
-const book1Elt = document.querySelector('.book1')
-const starkAllegiance = document.querySelector('.stark')
-const lannisterElt = document.querySelector('.lannister')
+const body = document.querySelector('body')
 
-const fetchData = () => {
-    fetch("https://www.anapioficeandfire.com/api/books/1")
-        .then((response) => response.json())
-        .then((data) => {
-            data.characters.forEach(character => {
-                fetch(character)
-                    .then((response1) => response1.json())
-                    .then(characterData => {
-                        
-                        if(characterData.allegiances.includes("https://anapioficeandfire.com/api/houses/362")
-                        || characterData.allegiances.includes("https://www.anapioficeandfire.com/api/houses/362")) {
-                            const li = document.createElement('li')
-                            li.textContent = characterData.name || characterData.aliases[0]
-                            starkAllegiance.appendChild(li)
-                        }
-
-                    })
-                    .catch((error) => {throw error})
-            })
-        })
-        .catch(error => { throw error })
-}
-
-fetchData()
+fetch("https://restcountries.com/v3.1/all")
+.then(response => response.json())
+.then(data => {
+    data.forEach( pays => {
+        console.log(pays)
+        if (pays.ccn3){
+            let imgSrc =`https://countryflagsapi.com/png/${pays.ccn3}`
+            const img = document.createElement('img')
+            img.src = imgSrc
+            body.appendChild(img)
+        } else throw error
+    })
+})
+.catch(error => { throw error})
