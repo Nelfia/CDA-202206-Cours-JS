c5f3a3baf9c20d4f61affc2ff9efc2da3537d734
Nelfia <qdecapele@gmail.com>
2022-09-12 16:33:55 +0200

    Correction beug

--- app.js
+++ app.js
@@ -5,22 +5,18 @@ const lannisterElt = document.querySelector('.lannister')
 const fetchData = () => {
     fetch("https://www.anapioficeandfire.com/api/books/1")
         .then((response) => response.json())
-        .then(bookData => {
-            bookData.characters.forEach(character => {
+        .then((data) => {
+            data.characters.forEach(character => {
                 fetch(character)
                     .then((response1) => response1.json())
                     .then(characterData => {
-                        const liElt = document.createElement('li')
-                        let name = characterData.name ? characterData.name : characterData.aliases[0]
-                        liElt.textContent = name
-                        book1Elt.appendChild(liElt)
-
-                        let allegeance = characterData.allegiances
-
-                        if (allegeance[0] === "https://anapioficeandfire.com/api/houses/362") {
-                            starkAllegiance.appendChild(liElt)
-                        } 
-                        else if ((allegeance[0] === "https://anapioficeandfire.com/api/houses/229") && !character.died) lannisterElt.appendChild(liElt)
+                        
+                        if(characterData.allegiances.includes("https://anapioficeandfire.com/api/houses/362")
+                        || characterData.allegiances.includes("https://www.anapioficeandfire.com/api/houses/362")) {
+                            const li = document.createElement('li')
+                            li.textContent = characterData.name || characterData.aliases[0]
+                            starkAllegiance.appendChild(li)
+                        }
 
                     })
                     .catch((error) => {throw error})
