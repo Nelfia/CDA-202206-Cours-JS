33fbfbed6c9fd526f967af8221d64397927d72d3
Nelfia <qdecapele@gmail.com>
2022-09-13 16:01:10 +0200

    Afficher les coordonnées de la station spatiale ISS et les actualiser toutes les 5 secondes. Exo OK

--- app.js
+++ app.js
@@ -1,71 +1,13 @@
-const article = document.querySelector('article')
-const input = document.querySelector('input')
-const button = document.querySelector('button')
 const ul = document.querySelector('ul')
 
-let fetchURL;
-let barCodeList = [];
-
-// Récupération et affichage de la liste des codes-barre
-if(localStorage.barCodeList) {
-    console.log(localStorage)
-    let barCodeListLS = JSON.parse(localStorage.barCodeList)  
-    if(barCodeListLS.length > 0) {
-        barCodeListLS.forEach( barCode => {
-            const li = document.createElement('li')
-            li.textContent = barCode
-            ul.appendChild(li)
-        }) 
-    }
-}
-
-
-const fetchData = () => {
-    if(!input.value) throw 'input vide'    
-    article.innerHTML = ""
-    fetchURL = `https://world.openfoodfacts.org/api/v0/product/${input.value}.json`
-    barCodeList.push(input.value)
-    localStorage.setItem("barCodeList", JSON.stringify(barCodeList))
-
-    fetch(fetchURL)
-    .then(response => response.json())
-    .then(data => {
-        data = data
-        console.log(data)
-        let h2 = document.createElement('h2')
-        h2.innerHTML = data.product.product_name_fr
-        article.appendChild(h2)
-        let div = document.createElement('div')
-        div.classList.add('images')
-        article.appendChild(div)
-        let img = document.createElement('img')
-        img.src = data.product.image_url
-        div.appendChild(img)
-        let ul = document.createElement('ul')
+fetch('http://api.open-notify.org/iss-now.json')
+.then(response => response.json())
+.then(data => {
+    setInterval(() => {
         ul.innerHTML = `
-            <li><h3>Allergènes :</h3> ${data.product.allergens_imported}</li>
-            <li><h3>Nutri-score :</h3> ${data.product.nutriscore_score}</li>
-            <li><h3>Macro-nutriments :</h3> 
-                <ul>
-                    <li><h4>Glucides :</h4> ${data.product.nutriments.carbohydrates}g pour 100g</li>
-                    <li><h4>Lipides :</h4> ${data.product.nutriments.fat}</li>
-                    <li><h4>Protéines :</h4> ${data.product.nutriments.proteins}</li>
-                </ul>
-            </li>
-            <li><h3>kcal :</h3> ${data.product.nutriments['energy-kcal']}</li>
-            <li><h3>Ingrédients :</h3> ${data.product.ingredients_text_fr}</li>
+            <li>Longitude : ${data.iss_position.longitude}</li>
+            <li>Latitude : ${data.iss_position.latitude}</li>
         `
-        article.appendChild(ul)
-    })
-    .catch(error => {throw error})
-
-
-}
-
-input.onkeydown = (e) => {
-    if(e.key === "Enter") fetchData()
-}
-
-button.onclick = () => {
-    fetchData()
-}
+    }, 5000)
+})
+.catch(error => {throw error})
