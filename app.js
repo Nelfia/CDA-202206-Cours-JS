e1f65b40c358a225ff79ea327ca951e2a813d068
Nelfia <qdecapele@gmail.com>
2022-09-13 10:13:55 +0200

    Même exercice avec l'utilisation de Async et Await

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const body = document.querySelector('body')
 
-fetch("https://restcountries.com/v3.1/all")
-.then(response => response.json())
-.then(data => {
-    data.forEach( pays => {
-        console.log(pays)
-        if (pays.ccn3){
-            let imgSrc =`https://countryflagsapi.com/png/${pays.ccn3}`
-            const img = document.createElement('img')
-            img.src = imgSrc
-            body.appendChild(img)
-        } else throw error
-    })
-})
-.catch(error => { throw error})
+const fetchData = async () => {
+    let data2;
+    await fetch("https://restcountries.com/v3.1/all")
+        .then(response => response.json())
+        .then(data => data2 = data)
+        .catch(error => { throw error })
+
+        data2.forEach( pays => {
+            console.log(pays)
+            if (pays.ccn3) {
+                let imgSrc = `https://countryflagsapi.com/png/${pays.ccn3}`
+                const img = document.createElement('img')
+                img.src = imgSrc
+                img.title = pays.name.official
+                body.appendChild(img)
+            } else {throw error}
+        })
+        console.log(data2)
+}
+fetchData()
