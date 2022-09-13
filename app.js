95d8d53a96bebdf25ca5b3a0773525a8ee7f17f5
Nelfia <qdecapele@gmail.com>
2022-09-13 21:56:02 +0200

    Ajout des commentaires

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-const jokeApiURL = 'https://v2.jokeapi.dev/joke/'
 let categorieURL = ''
 let fetchURL = 'https://v2.jokeapi.dev/joke/Any'
 
+const jokeApiURL = 'https://v2.jokeapi.dev/joke/'
 const colorPalette = ['#ffbd64', '#fff151', '#052ce3', '#087dff', '#b0e0fa', 'grey']
 
 const resultContainer = document.querySelector('#result-container')
 const buttonElt = document.querySelector('button')
 const categorieElts = document.querySelectorAll('.categorie')
 
-// Change le style des inputs de type checkbox
-const changerStyleCheckboxes = (tableau) => {
+// Change la couleur des bordures des inputs de type checkbox
+const colorCheckboxes = (tableau) => {
     tableau.forEach((element, index) => {
-        // Distribue une couleur differente à chaque catégorie et l'enregistre dans l'élément
-        let couleur = colorPalette[index]
-        element.dataset.colorPalette = couleur
-        element.style.borderColor = couleur
+        let couleur = colorPalette[index]   // // Distribue une couleur differente à chaque catégorie
+        element.dataset.colorPalette = couleur // l'enregistre dans l'attribut dataset.colorPalette
+        element.style.borderColor = couleur //  l'applique sur la bordure
     })
 }
-
-// Met à jour le style des checkboxes ciblées
+// Met à jour le style des labels des checkboxes ciblées
 const majStyleCheckbox = (e) => {
     // Modification du style des labels lorsque check ou non
-    if (!(e.target.control.checked)) {
+    if (!(e.target.control.checked)) { 
         e.target.style.backgroundColor = e.target.dataset.colorPalette
         e.target.style.color = 'white'
     } else {
@@ -29,20 +27,20 @@ const majStyleCheckbox = (e) => {
         e.target.style.color = e.target.dataset.colorPalette
     }
 }
-
+// Retourne une URL à partir d'un tableau de labels de checkboxes
 const handleCustomURL = (tableau) => {
-    categorieURL = ""
+    let url = ""
     tableau.forEach( filtre => {
-        if (filtre.control.checked) categorieURL += filtre.control.value + ','
+        if (filtre.control.checked) url += filtre.control.value + ','
     })
-    categorieURL = categorieURL.slice(0,-1)
+    return url.slice(0,-1)
 }
-
+// Récupération de la data en fonction de fetchURL
 const fetchData = (fetchURL) => {
     fetch(fetchURL)
     .then(response => response.json())
     .then(data => {
-        resultContainer.innerHTML = ''
+        resultContainer.innerHTML = ''  // Vide resultContainer avant d'y insérer la réponse
         console.log(data)
         if(data.type === 'single') {
             resultContainer.textContent = data.joke
@@ -56,24 +54,29 @@ const fetchData = (fetchURL) => {
                 resultContainer.appendChild(div2)
             }, 2000)
         }
-
-        
     })
     .catch(error => {throw error})
 }
 
-changerStyleCheckboxes(categorieElts)
 
+
+// ***********************  PROGRAMME  ***********************
+
+// STYLE : colore les checkboxes des categories
+colorCheckboxes(categorieElts)
+
+// STYLE : Au click sur chaque catégorie
 categorieElts.forEach(categorie => {
     categorie.onclick = (e) => {
-        majStyleCheckbox(e)
+        majStyleCheckbox(e) // change le style du label
     }
     categorie.control.checked = false
 })
 
+// Génère une blague en fonction des filtres sélectionnés
 buttonElt.onclick = (e) => {
     e.preventDefault()
-    handleCustomURL(categorieElts)
+    categorieURL = handleCustomURL(categorieElts)
     fetchURL = jokeApiURL + (categorieURL ? categorieURL : 'Any')
     fetchData(fetchURL)
     console.log(fetchURL)
